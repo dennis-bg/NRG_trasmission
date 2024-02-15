63c8e6cec0910bbf4550e629a4805bbacba34852
Dennis <dennis.galinsky@gmail.com>
2024-02-15 16:31:08 -0500

    added more acceleration and deceleration logic, need to fine  tune

--- automobiles/Automatic.py
+++ automobiles/Automatic.py
@@ -1,4 +1,7 @@
+import time
+
 from NRG_trasmission.automobiles.Automobile import Automobile
+from NRG_trasmission.automobiles.Gear import Gear
 
 
 class Automatic(Automobile):
@@ -8,9 +11,50 @@ class Automatic(Automobile):
         self.redLineRevs = redLineRevs
         super().__init__(name, noGears)
 
-    def accelerate(self):
-        pass
+    def accelerate(self, targetSpeed):
+        while self.currSpeed < targetSpeed and self.currGear.value <= self.noGears:
+            print(f"\nCurrent Gear: {self.currGear.name}")
+            while self.currSpeed < targetSpeed and self.currRevs < self.redLineRevs:
+                self.currRevs += 500
+                self.currSpeed += 2
+                print(f"Current Revs: {self.currRevs}")
+                print(f"Current Speed: {self.currSpeed}")
+                time.sleep(0.5)
+            try:
+                self.upShift()
+                self.currRevs = 1500
+            except Exception as e:
+                print(e)
+                break
 
-    def decelerate(self):
-        pass
+    def decelerate(self, targetSpeed=0):
+        while self.currSpeed > targetSpeed and self.currGear.value >= 0:
+            print(f"\nCurrent Gear: {self.currGear.name}")
+            while self.currSpeed > targetSpeed and self.currRevs > 2000:
+                self.currRevs -= 1000
+                self.currSpeed -= 5
+                print(f"Current Revs: {self.currRevs}")
+                print(f"Current Speed: {self.currSpeed}")
+                time.sleep(0.5)
+            try:
+                self.downShift()
+                self.currRevs = self.redLineRevs
+            except Exception as e:
+                print(e)
+                break
 
+    def drive(self):
+        self.currGear = Gear.FIRST
+        self.accelerate(45)
+        self.decelerate(10)
+
+
+def main():
+    automatic_car = Automatic("Toyota", 5, 6000)
+    print("Starting acceleration...")
+    automatic_car.drive()
+    print("Acceleration finished.")
+
+
+if __name__ == "__main__":
+    main()
--- automobiles/Automobile.py
+++ automobiles/Automobile.py
@@ -7,33 +7,30 @@ class Automobile(ABC):
 
     def __init__(self, name, noGears):
         self.currGear = Gear.NEUTRAL
+        self.currSpeed = 0
         self.noGears = noGears
         self.name = name
 
     def upShift(self):
-        if self.currGear == self.noGears:
-            print("You are in the highest gear")
+        if self.currGear.value == self.noGears:
+            raise Exception("You are in the highest gear")
         else:
-            self.currGear = self.currGear + 1
+            self.currGear = Gear(self.currGear.value + 1)
 
     def downShift(self):
-        if self.currGear == Gear.NEUTRAL:
-            print("You are in Neutral")
-        elif self.currGear == Gear.REVERSE:
-            print("You are in Reverse")
+        if self.currGear == Gear.NEUTRAL or self.currGear == Gear.REVERSE:
+            raise Exception(f"You are in {self.currGear.name}")
         else:
-            self.currGear = self.currGear - 1
+            self.currGear = Gear(self.currGear.value - 1)
 
     def setNeutral(self):
         self.currGear = Gear.NEUTRAL
+        print("You are in Neutral")
 
     def setReverse(self):
         self.currGear = Gear.REVERSE
+        print("You are in Reverse")
 
     @abstractmethod
-    def accelerate(self):
-        pass
-
-    @abstractmethod
-    def decelerate(self):
+    def drive(self):
         pass
